1e601185385bfe660dd7bfc5f77f9113462c51ca
Oier Echarri <oier.echarri@opendeusto.es>
2018-04-30 19:55:33 +0200

    MenuWindow-><-Select Soup->Solve Soup

--- LetterSoup-App/Client/src/main/java/es/deusto/spq/window/SelectSoup.java
+++ LetterSoup-App/Client/src/main/java/es/deusto/spq/window/SelectSoup.java
@@ -14,8 +14,14 @@ import javax.swing.border.EmptyBorder;
 import es.deusto.spq.controller.controller;
 
 import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SelectSoup extends JFrame {
+import javax.swing.JButton;
+
+public class SelectSoup extends JFrame implements Runnable{
 /**
  * Window that shows all soups and allow user to select one of them. 
  */
@@ -27,12 +33,17 @@ public class SelectSoup extends JFrame {
 	controller cont = null; 
 	private String[] args=null;
 	String[] listOfSoups;
+	private String us=null;
+	private String [] arg=null;
+	private String nameS=null;
+	
+	
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		SelectSoup frame = new SelectSoup(args);
-		frame.setVisible(true);
+//	public static void main(String[] args) {
+//		SelectSoup frame = new SelectSoup(args, nameSoup, user);
+//		frame.setVisible(true);
 //		EventQueue.invokeLater(new Runnable() {
 //			public void run() {
 //				try {
@@ -42,13 +53,16 @@ public class SelectSoup extends JFrame {
 //				}
 //			}
 //		});
-	}
+//	}
 
 	/**
 	 * Create the frame.
 	 */
-	public SelectSoup(String[] args) {
+	public SelectSoup(String[] args,String nameSoup, String user) {
 		this.args=args;
+		this.arg=args;
+		this.us=user;
+		this.nameS=nameSoup;
 		try {
 			cont=new controller(args);
 		} catch (RemoteException e) {
@@ -64,7 +78,7 @@ public class SelectSoup extends JFrame {
 		contentPane.setLayout(null);
 		
 		panel = new JPanel();
-		panel.setBounds(0, 34, 250, 227);
+		panel.setBounds(10, 26, 258, 224);
 		contentPane.add(panel);
 		
 		//insert here the method that extracts all the soups name from database
@@ -83,5 +97,49 @@ public class SelectSoup extends JFrame {
         //Creating the JList and inserting the model on it
         list = new JList<String>(modelo);
 		panel.add(list);
+		
+		JLabel lblSelectTheSoup = new JLabel("Select a soup from the list: ");
+		lblSelectTheSoup.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSelectTheSoup.setBounds(10, 11, 228, 14);
+		contentPane.add(lblSelectTheSoup);
+		
+		JButton btnSelect = new JButton("Select");
+		btnSelect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SolveSoup ss = new SolveSoup(arg,nameS,us);
+				ss.setVisible(true);
+			}
+		});//For the moment it can not select the soup 
+		btnSelect.setBounds(278, 26, 146, 23);
+		contentPane.add(btnSelect);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnSelect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MenuWindow mw = new MenuWindow(arg,nameS,us);
+				mw.setVisible(true);
+			}
+		});
+		btnCancel.setBounds(278, 60, 146, 23);
+		contentPane.add(btnCancel);
 	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setCont(controller cont) {
+		this.cont = cont;
+	}
+
+	public String[] getListOfSoups() {
+		return listOfSoups;
+	}
+
+	public void setListOfSoups(String[] listOfSoups) {
+		this.listOfSoups = listOfSoups;
+	}
+	public controller getCont() {
+	return cont;
+}
 }
