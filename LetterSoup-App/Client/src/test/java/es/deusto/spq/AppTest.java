4e8d3ef217a89f9371708f19298ee93a51ec4c90
gaizka <gaizka.ziorraga@opendeusto.es>
2018-05-03 03:23:29 +0200

    client side junit without proving it

--- LetterSoup-App/Client/src/test/java/es/deusto/spq/AppTest.java
+++ LetterSoup-App/Client/src/test/java/es/deusto/spq/AppTest.java
@@ -7,6 +7,13 @@ import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
 
+import es.deusto.spq.controller.controller;
+import es.deusto.spq.window.ExamMode;
+import es.deusto.spq.window.LoginWindow;
+import es.deusto.spq.window.MenuWindow;
+import es.deusto.spq.window.PointsWindow;
+import es.deusto.spq.window.Register;
+import es.deusto.spq.window.SelectSoup;
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestCase;
 
@@ -16,7 +23,14 @@ import junit.framework.TestCase;
 public class AppTest extends TestCase
 {
 	
-
+private controller cont;
+private LoginWindow lw;
+private MenuWindow mw;
+private Register r;
+private SelectSoup sp;
+private PointsWindow pw;
+private ExamMode em;
+private String[] args= {"127.0.0.1","1099","LetterSoupServer"};
 
 	
     /**
@@ -28,7 +42,16 @@ public class AppTest extends TestCase
     /**
      * @return the suite of tests being tested
      */
+@Before public void setup() {
+	cont = new controller();
+	lw = new LoginWindow(args);
+	mw = new MenuWindow(args,null,"a1");
+	r = new Register();
+	sp = new SelectSoup(args,null,"a1");
+	pw = new PointsWindow(args);
+	em = new ExamMode();
 	
+}
    // public static Test suite()
     //{
        // return new TestSuite( AppTest.class );
@@ -40,7 +63,12 @@ public class AppTest extends TestCase
      */
 	@Test public void testApp() throws RemoteException
     {
-      
+		controller.getController();
+		controller.getController().setController(args);
+		controller.getController().login("a1", "abc");
+		controller.getController().soupList();
+		controller.getController().setController(args);
+        lw.main(args);
 		
     }
 	
